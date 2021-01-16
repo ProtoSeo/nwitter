e1f869ea04fd87246de30eb53639c86d3ae69ebf
SeoSeunghun <proto_seo@naver.com>
2021-01-16 14:33:32 +0900

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Hc2dQg9bDzZLG2OjE5Hzy4rGNqVFQrA",
+  authDomain: "nwitter-6023c.firebaseapp.com",
+  projectId: "nwitter-6023c",
+  storageBucket: "nwitter-6023c.appspot.com",
+  messagingSenderId: "861585224225",
+  appId: "1:861585224225:web:749fe2884d468814312960"
+};
+export default firebase.initializeApp(firebaseConfig);
